9a177c71016081d5f6ab90d782e35ec2d619b525
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-02-12 18:22:42 -0500

    Created Gambler, with fields and methods

---
+++ src/com/games/forever21/blackjack/Gambler.java
@@ -0,0 +1,26 @@
+package com.games.forever21.blackjack;
+
+abstract class Gambler {
+    private String name;
+    private Long balance;
+    private Collection<Hand> currentHand;
+    private boolean hasWon;
+    private boolean hasLost;
+    private Long amountOfWins;
+
+    public void countHand() {
+
+    }
+
+    public void hit() {
+
+    }
+
+    public void pass() {
+
+    }
+
+    public void currentBalance() {
+
+    }
+}
