d00a681998fdcae84c5702ece4eb58929ffd514b
Amin <amin747@gmail.com>
2021-02-15 15:23:26 -0500

    maps created for Table class

--- src/com/games/forever21/blackjack/controller/Table.java
+++ src/com/games/forever21/blackjack/controller/Table.java
@@ -2,8 +2,20 @@ package com.games.forever21.blackjack.controller;
 
 import com.games.forever21.blackjack.domain.Gamblers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Table {
     private Gamblers gamblers;
+
+    //map to store gambler and their balances in total
+    Map<Gamblers, Integer> gamblersTotalBalance = new HashMap<>();
+
+    //map to store gamblers bets in the current game
+    Map<Gamblers, Integer> bets = new HashMap<>();
+
+
+
 
     public void startGame() {
 
