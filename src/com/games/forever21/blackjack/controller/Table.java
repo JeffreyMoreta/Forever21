c5fb494a669518bce528c6d099e75a1909d3d992
Danny Manandhar <63838054+ALOHAWORLD7@users.noreply.github.com>
2021-02-15 12:59:25 -1000

    Add Prompter for the table.

--- src/com/games/forever21/blackjack/controller/Table.java
+++ src/com/games/forever21/blackjack/controller/Table.java
@@ -1,15 +1,42 @@
 package com.games.forever21.blackjack.controller;
 
+import com.apps.util.Prompter;
 import com.games.forever21.blackjack.domain.Gamblers;
 
-class Table {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Table {
+
+    private Prompter prompter;
     private Gamblers gamblers;
 
-    public void startGame() {
+
+    public Table(Prompter prompter){
+        this.prompter = prompter;
+    }
+
+    public void startGame() throws IOException {
+        showBanner();
+        String name = prompter.prompt("Please enter name: ");
+        System.out.println("Welcome to the game: " + name );
+
+
+        // ask for name 2
+        // create gambler object
+        // likewise
+        // add it to map
+
+        //
 
     }
 
-    public void endGame() {
+    private void showBanner() throws IOException {
+        String banner = Files.readString(Path.of("resources/banner.txt"));
+        prompter.info(banner);
+    }
 
+    public void endGame() {
     }
 }
